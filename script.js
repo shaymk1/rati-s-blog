176bfe4293fddc843990cc95da19d73826e74b6c
Mamosebetsi Kekae <69590144+shaymk1@users.noreply.github.com>
2024-06-29 16:39:30 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+//********** ***// hamburger menu //********** ***//
+
+const navMenu = document.querySelector(".nav__menu");
+const navBtnOpen = document.querySelector(".nav__toggle-open");
+const navBtnClose = document.querySelector(".nav__toggle-close");
+const openNavHandler = () => {
+	navMenu.style.display = "flex";
+	navBtnOpen.style.display = "none";
+	navBtnClose.style.display = "inline-block";
+};
+
+const closeNavHandler = () => {
+	navMenu.style.display = "none";
+	navBtnOpen.style.display = "inline-block";
+	navBtnClose.style.display = "none";
+};
+
+navBtnOpen.addEventListener("click", openNavHandler);
+navBtnClose.addEventListener("click", closeNavHandler);
+
+// close the nav menu on click of the nav link on small screens
+const navItems = navMenu.querySelectorAll("a");
+if (window.innerWidth < 768) {
+	navItems.forEach((item) => {
+		item.addEventListener("click", closeNavHandler);
+	});
+}
+
+
+
+const themeBTN = document.querySelector(".nav__theme-btn");
+const body = document.querySelector(".dark-mode");
+themeBTN.addEventListener('click', ()=>{
+body.classList.toggle("active")
+})
